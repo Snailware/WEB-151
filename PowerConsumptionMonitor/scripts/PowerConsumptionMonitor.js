0bbcc59dc6c97f5a6702e1136c958c6ea57f2556
Snailware <Adamlancaster.al@gmail.com>
2021-11-28 11:41:26 -0500

    added funcs. in prog.

--- PowerConsumptionMonitor/scripts/PowerConsumptionMonitor.js
+++ PowerConsumptionMonitor/scripts/PowerConsumptionMonitor.js
@@ -51,3 +51,196 @@ function submitPassword()
         alert("Incorrect password, please try again.");
     }
 }
+
+// submit plant infoto local storage.
+function submitPlantForm() 
+{
+    savePlantForm();
+    return true;
+}
+
+// save plant info to local storage. 
+function savePlantForm()
+{
+    if(checkPlantForm())
+    {
+        // plant object to be saved. 
+        var plant = 
+        {
+            "PlantID" : $("#txtPlantID").val(),
+            "InstallDate" : $("#installDate").val(),
+            "NewPassword" : $("#changePassword").val()
+        };
+
+        try 
+        {
+            localStorage.setItem("plant", JSON.stringify(plant));
+            alert("Saving Information");
+            $.mobile.changePage("#pageMenu");
+        }
+        catch(e)
+        {
+            if(window.navigator.vendor === "Google Inc") 
+            {
+                if(e == DOMException.QUOTA_EXCEEDED_ERR)
+                {
+                    alert("Error: Local Storage limit exceeded.");
+                }
+            }
+            else if(e == QUOTA_EXCEEDED_ERR)
+            {
+                alert("Error: Saving to local storage.");
+            }
+
+            console.log(e);
+        }
+    }
+    else
+    {
+        alert("Please complete the form properly.");
+    }
+}
+
+// check form fields for validity.
+function checkPlantForm()
+{
+    var d = new Date();
+    var month = d.getMonth() + 1;
+    var date = d.getDate();
+    var year = d.getFullYear();
+    var currentDate = year + '/' + 
+    (('' + month).length < 2 ? '0' : '') + month + '/' +
+    (('' + date).length < 2 ? '0' : '') + date;
+
+    if(($("#txtPlantID").val() != "") &&
+        ($("#installDate").val() != "") &&
+        ($("#installDate").val() <= currentDate))
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+
+// populate stored plant obj data in form.
+function showPlantForm()
+{
+    try
+    {
+        var plant = JSON.parse(localStorage.getItem("plant"));
+    }
+    catch(e) 
+    {
+        if(window.navigator.vendor === "Google Inc") 
+        {
+            if(e == DOMException.QUOTA_EXCEEDED_ERR)
+            {
+                alert("Error: Local Storage limit exceeded.");
+            }
+        }
+        else if(e == QUOTA_EXCEEDED_ERR)
+        {
+            alert("Error: Saving to local storage.");
+        }
+
+        console.log(e);
+    }
+
+    if(plant != null)
+    {
+        $("#txtPlantID").val(plant.PlantID);
+        $("#installDate").val(plant.InstallDate);
+        $("#changePassword").val(plant.NewPassword);
+    }
+}
+
+// displays information for each page as they are shown.
+$(document).on("pageshow", function()
+{
+    if($('.ui-page-active').attr('id')=="pagePlantInfo")
+    {
+        showPlantForm();
+    }
+    else if($('.ui-page-active').attr('id')=="pagePlantRecords")
+    {
+        loadPlantInformation();
+        //listRecords();
+    }
+    else if($('.ui-page-active').attr('id')=="pagePlantRecommendations")
+    {
+        advicePage();
+        resizeGraph();
+    }
+    else if($('.ui-page-active').attr('id')=="pagePlantGraph")
+    {
+        drawGraph();
+        resizeGraph();
+    }
+});
+
+// display plant info on records page.
+function loadPlantInformation()
+{
+    try
+    {
+        var plant = JSON.parse(localStorage.getItem("plant"));
+    }
+    catch(e) 
+    {
+        if(window.navigator.vendor === "Google Inc") 
+        {
+            if(e == DOMException.QUOTA_EXCEEDED_ERR)
+            {
+                alert("Error: Local Storage limit exceeded.");
+            }
+        }
+        else if(e == QUOTA_EXCEEDED_ERR)
+        {
+            alert("Error: Saving to local storage.");
+        }
+
+        console.log(e);
+    }
+
+    if(plant != null)
+    {
+        $("#divPlantSection").empty();
+
+        $("#divPlantSection").append("Plant ID:" + plant.PlantID + "<br/>" +
+                                     "Install Date:" + plant.InstallDate + "<br/>" +
+                                     "New Password:" + plant.NewPassword);
+                        
+        $("#divPlantSection").append("<br/><a href='#pagePlantInfo' data-mini='true' id='btnProfile' data-role='button' data-icon='edit' data-iconpos='left' data-inline='true'>Edit Profile</a>");
+        $('#btnProfile').button();
+    }
+
+
+}
+
+// triggers new record page.
+$("btnAddRecord").click(function()
+{
+    $("btnSubmitRecord").val("Add");
+    if($("#btnSubmitRecord").hasClass("btn-ui-hidden"))
+    {
+        $("#btnSubmitRecord").button("refresh");
+    }
+});
+
+// initiates addition of record.
+$("#pageNewRecordForm").on("pageshow", function()
+{
+    var formOperation = $("#btnSubmitRecord").val();
+
+    if(formOperation == "Add")
+    {
+        clearRecordForm();
+    }
+    else if(formOperation == "Edit")
+    {
+        showRecordForm($("btnSubmitRecord").attr("indexToEdit"));
+    }
+});
+
